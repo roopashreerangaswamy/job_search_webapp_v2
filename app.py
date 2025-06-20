72f669554f3c8c2439f25cbcfe49732966d1a430
roopashreerangaswamy <151712287+roopashreerangaswamy@users.noreply.github.com>
2025-06-20 17:07:13 +0000

    added html

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+jobs = [
+    {
+        "title": "Software Engineer",
+        "company": "CodeWorks",
+        "location": "Bengaluru",
+        "type": "Full-Time"
+    },
+    {
+        "title": "Marketing Analyst",
+        "company": "BrightAds",
+        "location": "Mumbai",
+        "type": "Internship"
+    },
+    {
+        "title": "Graphic Designer",
+        "company": "Creatify",
+        "location": "Remote",
+        "type": "Freelance"
+    }
+]
 @app.route('/') 
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html',jobs=jobs)
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',debug=True)
