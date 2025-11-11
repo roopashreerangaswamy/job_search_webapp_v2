87cdfea22153679a087f0aaf764c65d1648989db
roopashreerangaswamy <151712287+roopashreerangaswamy@users.noreply.github.com>
2025-11-11 09:20:59 +0000

    major changes and features added

--- app.py
+++ app.py
@@ -56,12 +56,7 @@ def apply_to_job(job_id):
 
 
 
-@app.route('/job/<id>')
-def show_job(id):
-    job = load_job_from_db(id)
-    if not job:
-        return "Not Found", 404
-    return render_template('jobpage.html', job=job)
+
 
 
 
@@ -118,6 +113,15 @@ def register():
             return redirect('/register')
 
     return render_template('register.html')
+
+
+
+@app.route('/job/<id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html', job=job)
 
 
 @app.route('/for_recruiters')
@@ -308,6 +312,11 @@ def view_applications(job_id):
 
     return render_template('recruiter_applications.html', job=job, applications=applications)
 
+@app.route('/delete_application/<app_id>/<job_id>', methods=['POST'])
+def delete_application(app_id, job_id):
+    db.applications.delete_one({'_id': ObjectId(app_id)})
+    flash('Application deleted successfully!', 'success')
+    return redirect(url_for('view_applications', job_id=job_id))
 
 
 
